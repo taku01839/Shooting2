c310e019854b8d92584fafa2b19ad324fef32cd8
taku01839 <taku01839@gmail.com>
2019-06-18 09:33:59 +0900

    自機と敵をImageの外に

---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public Slider _slider;
+    [SerializeField]
+    private GameObject bullet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //弾発射
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
